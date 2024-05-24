b896200f6ac3d0ce11d757467443e309af3c397d
CodingManoj <CodingManoj@gmail.com>
2024-05-23 20:02:22 -0400

    Auto Push

--- eks/data.tf
+++ eks/data.tf
@@ -2,7 +2,7 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket  = "b57-terraform-state-bucket"
+    bucket  = "b57-tf-state-bucket"
     key     = "${var.ENV}/vpc/terraform.tfstate"
     region  = "us-east-1"
   }
