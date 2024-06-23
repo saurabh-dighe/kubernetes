3be41447d83033de36fddbcf748d541971b8920f
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-06-23 14:25:43 +0530

    Auto push

--- eks/main.tf
+++ eks/main.tf
@@ -1,8 +1,8 @@
 module "eks" {
   source             = "./vendor/modules/eks"
   ENV                = var.ENV
-  PRIVATE_SUBNET_IDS = ["subnet-0dbf40ba94ccc319e", "subnet-06c85d0801d6571a9"] #data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
-  PUBLIC_SUBNET_IDS  = ["subnet-09f0236c4b3694cb9", "subnet-0cd3ab59c9c3879f6"] #data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_IDS
+  PRIVATE_SUBNET_IDS = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS #["subnet-0dbf40ba94ccc319e", "subnet-06c85d0801d6571a9"] 
+  PUBLIC_SUBNET_IDS  = data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_IDS #["subnet-09f0236c4b3694cb9", "subnet-0cd3ab59c9c3879f6"] 
   DESIRED_SIZE       = 1
   MAX_SIZE           = 1
   MIN_SIZE           = 1
